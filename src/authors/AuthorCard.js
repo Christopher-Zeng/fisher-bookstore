4ca0f3131d61ff74c8ec58c3eb4e146b92e3c6de
Christopher Zeng <zeng.519@osu.edu>
2020-03-06 16:02:31 -0500

    Add accordion display component

---
+++ src/authors/AuthorCard.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Card, Button, Accordion } from "react-bootstrap";
+export function AuthorCard(props) {
+    return (
+        <Card>
+            <Card.Header>
+                <Accordion.Toggle as={Card.Header} eventKey={props.author.id}>
+                    {props.author.name}
+                </Accordion.Toggle>
+            </Card.Header>
+            <Accordion.Collapse eventKey={props.author.id}>
+                <Card.Body>
+                    <Card.Text>{props.author.description}</Card.Text>
+                    <Button variant="warning">Explore</Button>
+                </Card.Body>
+            </Accordion.Collapse>
+        </Card>
+    )
+}
